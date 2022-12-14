69403b0705614f9be0a471356a69c1daa54e8f04
Saman Mahalati <mahalatisaman@gmail.com>
2022-12-14 16:57:34 +0330

    Remove the coin table from home page for better performance

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 //components
 import Landing from './Landing';
-import Coins from "./Coins";
 
 const Home = () => {
     return (
         <main>
             <Landing/>
-            <Coins/>
         </main>
     );
 };
